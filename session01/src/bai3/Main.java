6925ffa6c6778ac650681de9103df7fe86317b29
Haidang2101 <haidanghanoi2003@gmail.com>
2026-03-08 23:54:41 +0700

    Add files via upload

---
+++ session01/src/bai3/Main.java
@@ -0,0 +1,13 @@
+package bai3;
+
+import java.util.Scanner;
+
+public class Main {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        b3 a=new b3();
+        a.inputData(sc);
+
+
+    }
+}
